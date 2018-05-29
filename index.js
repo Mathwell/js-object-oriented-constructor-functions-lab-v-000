369129060705d20172b90802e6fab28bddea7eb5
Olena Ageyeva <eageeva@hotmail.com>
2018-05-29 12:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ function Scooter(year,color,model){
   this.model=model;
 }
 
-function Driver(){
-  
+function Driver(name,age,experience){
+ this.name=name;
+ this.age=age;
+ this.experience=experience;
 }
 
 function PickupLocation(){
-  
+
 }
